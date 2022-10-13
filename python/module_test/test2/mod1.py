1bd2dcf9ba92190fa1c8aad06d5a3171b94f6848
mhgn <mhgn0001@gmail.com>
2022-10-13 22:46:06 +0530

    Day-11 Module And Packages Added

---
+++ python/module_test/test2/mod1.py
@@ -0,0 +1,12 @@
+def fun1():
+    print("fun1 is called")
+    return "fun1 is called"
+
+
+def fun2():
+    print("fun2 is called")
+    return "fun2 is called"
+
+def fun3():
+    print("fun3 is called")
+    return "fun3 is called"
---
+++ python/module_test/test2/mod2.py
@@ -0,0 +1,12 @@
+def fun21():
+    print("fun21 is called")
+    return "fun21 is called"
+
+
+def fun22():
+    print("fun22 is called")
+    return "fun22 is called"
+
+def fun23():
+    print("fun23 is called")
+    return "fun23 is called"
